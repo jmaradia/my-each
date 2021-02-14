9021458d9f53847cb8e36a5852b3ffd1690eabe7
josemaradiaga <josemarad25@gmail.com>
2021-02-14 09:03:53 -0500

    commit tree 565c95aa99a55743dd9cb3a69dea7a77b8083730

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
+def my_each (arguments)# put argument(s) here
   # code here
+i = 0
+while i < arguments.length
+  yield(arguments[i])
+  i = i + 1
+end
+arguments
 end
